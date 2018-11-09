808a1ede611a8e42c464b0f1783e715a6fae61dc
Thuận <nguyenbuiducthuan@gmail.com>
2018-11-09 15:37:34 +0700

    update listener and promise handle to avoid leak memory

--- android/src/main/java/com/nodejsaber/tts/SpeechSynthesizerModule.java
+++ android/src/main/java/com/nodejsaber/tts/SpeechSynthesizerModule.java
@@ -1,41 +1,37 @@
+
 package com.nodejsaber.tts;
 
-import java.util.Locale;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import android.content.Context;
-
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech.Engine;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.UtteranceProgressListener;
-
-import com.facebook.common.logging.FLog;
-
-import com.facebook.react.common.ReactConstants;
-
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.GuardedAsyncTask;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.common.logging.FLog;
+import com.facebook.react.bridge.GuardedAsyncTask;
+import com.facebook.react.common.ReactConstants;
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter;
 
+import java.util.Map;
+import java.util.Locale;
+import java.util.HashMap;
+import java.util.UUID;
+
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.content.Context;
+import android.annotation.TargetApi;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech.Engine;
 
 class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
-    private Context context;
+
     private static TextToSpeech tts;
+	private boolean IS_READY;
+    private Context context;
     private Map<String, Promise> ttsPromises = new HashMap<String, Promise>();
 
     public SpeechSynthesizerModule(ReactApplicationContext reactContext) {
@@ -44,28 +40,34 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
         this.init();
     }
 
-    /**
-     * @return the name of this module. This will be the name used to {@code require()} this module
-     * from javascript.
-     */
     @Override
     public String getName() {
         return "SpeechSynthesizer";
     }
 
-    /**
-     * Intialize the TTS module
-     */
+  
      public void init(){
         tts = new TextToSpeech(getReactApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if(status == TextToSpeech.ERROR){
+                
+                if(status == TextToSpeech.SUCCESS){
+					IS_READY = true;
+                } else {
+                    IS_READY = false;
                     FLog.e(ReactConstants.TAG,"Not able to initialized the TTS object");
                 }
             }
         });
         tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+                WritableMap map = Arguments.createMap();
+                map.putString("utteranceId", utteranceId);
+                getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
+                    .emit("StartSpeechUtterance", map);
+            }
+
             @Override
             public void onDone(String utteranceId) {
                 WritableMap map = Arguments.createMap();
@@ -73,8 +75,12 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
                 getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
                     .emit("FinishSpeechUtterance", map);
                 Promise promise = ttsPromises.get(utteranceId);
-                promise.resolve(utteranceId);
+                if (promise != null) {
+                    promise.resolve(utteranceId);
+                    ttsPromises.remove(utteranceId);
+                }
             }
+
 
             @Override
             public void onError(String utteranceId) {
@@ -83,16 +89,28 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
                 getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
                     .emit("ErrorSpeechUtterance", map);
                 Promise promise = ttsPromises.get(utteranceId);
-                promise.reject(utteranceId);
+                if (promise != null) {
+                    promise.reject(utteranceId);
+                    ttsPromises.remove(utteranceId);
+                } 
+
             }
 
             @Override
-            public void onStart(String utteranceId) {
+			public void onStop(String utteranceId, boolean interrupted) {
                 WritableMap map = Arguments.createMap();
                 map.putString("utteranceId", utteranceId);
                 getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
-                    .emit("StartSpeechUtterance", map);
-            }
+                .emit("StopSpeechUtterance", map);
+                Promise promise = ttsPromises.get(utteranceId);
+                if (promise != null) {
+                    promise.resolve(utteranceId);
+                    ttsPromises.remove(utteranceId);
+                }
+			}
+
+
+
         });
     }
 
@@ -197,6 +215,9 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
                     promise.reject("Text cannot be blank");
                 }
 
+                String speechUUID = UUID.randomUUID().toString();
+                ttsPromises.put(speechUUID, promise);
+
                 try {
                     if (voice != null && voice != "") {
                         tts.setLanguage(new Locale(voice));
@@ -210,16 +231,13 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
                     }
 
                     int speakResult = 0;
-                    String speechUUID = UUID.randomUUID().toString();
                     if(Build.VERSION.SDK_INT >= 21) {
                         Bundle bundle = new Bundle();
                         bundle.putCharSequence(Engine.KEY_PARAM_UTTERANCE_ID, "");
-                        ttsPromises.put(speechUUID, promise);
                         speakResult = tts.speak(text, queueMethod, bundle, speechUUID);
                     } else {
                         HashMap<String, String> map = new HashMap<String, String>();
                         map.put(Engine.KEY_PARAM_UTTERANCE_ID, speechUUID);
-                        ttsPromises.put(speechUUID, promise);
                         speakResult = tts.speak(text, queueMethod, map);
                     }
 
@@ -227,7 +245,11 @@ class SpeechSynthesizerModule extends ReactContextBaseJavaModule {
                         throw new Exception("Speak failed, make sure that TTS service is installed on you device");
                     }
                 } catch (Exception e) {
-                    promise.reject(e.getMessage());
+                    Promise promise = ttsPromises.get(speechUUID);
+                    if (promise != null) {
+                        promise.reject(e.getMessage());
+                        ttsPromises.remove(speechUUID);
+                    } 
                 }
             }
         }.execute();
